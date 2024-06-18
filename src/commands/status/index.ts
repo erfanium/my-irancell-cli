05fb20e07a1ce251d8937944f472c97c36bdd424
Erfanium <erfanshield@outlook.com>
2024-06-19 01:46:30 +0330

    some refactor

--- src/commands/status/index.ts
+++ src/commands/status/index.ts
@@ -1,45 +1,37 @@
-import CliTable from "cli-table3";
-
 import { AuthorizedCommand } from "../../command.js";
-import { TableCommon } from "../../table.js";
+import { makeTable } from "../../table.js";
+
+export const formatSize = (sizeInMegaBytes: number): string =>
+  `${(sizeInMegaBytes / 1024).toFixed(2)} GB`;
 
 export default class Status extends AuthorizedCommand {
   static description = "Show account status";
 
   async run(): Promise<void> {
-    const data = await this.api.getAccount();
-
-    const table = new CliTable({
-      ...TableCommon,
-      head: ["Offer Code", "Name", "Expiry", "Data Used", "Data Remaining"],
-      colWidths: [15, 30, 15, 15, 15],
+    const rtf = new Intl.RelativeTimeFormat("en", {
+      style: "long",
+      numeric: "auto",
     });
 
-    for (const offer of data.active_offers) {
-      const expiryDate = Date.parse(offer.expiry_date);
-      const now = Date.now();
+    const data = await this.api.getAccount();
 
-      //convert milliseconds to days
-      var daysRemaining = Math.round((expiryDate-now)/(1000*3600*24));
-
-      //if expired, days remaining set to zero
-      if(daysRemaining<0){
-        daysRemaining=0
-      }
-
-      const globalDataUsedGB = (offer.global_data_used / 1024).toFixed(2);
-      const globalDataRemainingGB = (
-        offer.global_data_remaining / 1024
-      ).toFixed(2);
-
-      table.push([
+    const table = makeTable({
+      head: ["Offer Code", "Name", "Expiry", "Data Used / Remaining"],
+      colWidths: [15, 30, 15, 30],
+      rows: data.active_offers.map((offer) => [
         offer.offer_code,
         offer.name,
-        `in ${daysRemaining} days`,
-        globalDataUsedGB + " GB",
-        globalDataRemainingGB + " GB",
-      ]);
-    }
+        rtf.format(
+          Math.round(
+            (Date.parse(offer.expiry_date) - Date.now()) / (1000 * 60 * 60 * 24)
+          ),
+          "day"
+        ),
+        `${formatSize(
+          offer.total_amount - offer.remained_amount
+        )} / ${formatSize(offer.total_amount)}`,
+      ]),
+    });
 
     this.log(table.toString());
   }
